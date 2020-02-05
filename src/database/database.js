3e05b271062328f0418814094a078e1078fe9a54
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2020-02-05 14:47:26 -0300

    problem with db

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,8 @@ var db_config = {
   host: 'us-cdbr-iron-east-04.cleardb.net',
   user: 'bca85caf769d95',
   password: 'cc74bf51',
-  database: 'heroku_498e6b6a2988f05'
+  database: 'heroku_498e6b6a2988f05',
+  multipleStatements: true
 };
 
 var mysqlConnection;
@@ -16,6 +17,9 @@ function handleDisconnect() {
     if(err) {                                     
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); 
+      return;
+    }else {
+      console.log('DB id connected');
     }                                     
   });                                     
   mysqlConnection.on('error', function(err) {
