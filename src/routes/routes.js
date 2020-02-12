d81bf5fbf76b86b9c6bf3bcb57fe1428566fcc05
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2020-02-12 17:44:14 -0300

    nodemailer

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,44 @@ router.get('/', (req, res) => {
     res.send('Servidor funcionando con exito');
 });
 
+// EMAIL SENDER -----------------------------------
+
+
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
+
+
+
+
+
+
+
+// EMAIL SENDER -----------------------------------
+
+
 
 // FILTERS ------------------------------------
 
