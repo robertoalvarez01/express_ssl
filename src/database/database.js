ee728ffe62db6409657de028a4f55138bda2c223
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2020-02-18 15:12:26 -0300

    error handler eliminated

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 
+
 var db_config = {
   host: 'localhost',
   user: 'root',
@@ -8,30 +9,16 @@ var db_config = {
   multipleStatements: true
 };
 
-var mysqlConnection;
+const mysqlConnection = mysql.createConnection(db_config);
 
-function handleDisconnect() {
-  mysqlConnection = mysql.createConnection(db_config); 
 
-  mysqlConnection.connect(function(err) {              
-    if(err) {                                     
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-      return;
-    }else {
-      console.log('DB id connected');
-    }                                     
-  });                                     
-  mysqlConnection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                         
-    } else {                                      
-      throw err;                                  
-    }
-  });
-}
-
-handleDisconnect();
+mysqlConnection.connect(function(err) {              
+  if(err) {                                     
+    console.log('error when connecting to db:', err); 
+    return;
+  }else {
+    console.log('DB id connected');
+  }                                     
+});                                     
 
 module.exports = mysqlConnection;
