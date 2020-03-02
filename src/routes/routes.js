bf23256515132080ea5e211b2fdce322f03e16d1
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2020-03-02 10:33:18 -0300

    filtro de moneda

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -369,10 +369,10 @@ router.get('/filtrar_ubicacion/:idLocalidad/:order', (req, res) => {
 
 
 
-router.get('/filtrar_todo/:idLocalidad/:idCategoria/:idOperacion/:order', (req, res) => {
-    const { idLocalidad, idCategoria, idOperacion, order }= req.params;
+router.get('/filtrar_todo/:idLocalidad/:idCategoria/:idOperacion/:order/:moneda', (req, res) => {
+    const { idLocalidad, idCategoria, idOperacion, order, moneda }= req.params;
     if(order == "normal"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ?', [idLocalidad, idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? AND moneda = ?', [idLocalidad, idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
@@ -409,7 +409,7 @@ router.get('/filtrar_todo/:idLocalidad/:idCategoria/:idOperacion/:order', (req, 
             }
         });
     }else if(order == "high"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? ORDER BY precio DESC', [idLocalidad, idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? AND moneda = ? ORDER BY precio DESC', [idLocalidad, idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
@@ -446,44 +446,7 @@ router.get('/filtrar_todo/:idLocalidad/:idCategoria/:idOperacion/:order', (req, 
             }
         });
     }else if(order == "low"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? ORDER BY precio ASC', [idLocalidad, idCategoria, idOperacion], (err, rows, fields) => {
-            if(! err){
-                casas = [];
-                rows.forEach(inmueble => {
-                    casas.push(inmueble.id);
-                })
-                db.query('SELECT * FROM imagenes WHERE idCasa = ? AND header = true', [casas], (error, imagen, celdas) => {
-                    if (! error){
-
-                        rows.forEach(propiedad => {
-                            imagen.forEach(header => {
-                                if (propiedad.id == header.idCasa){
-                                    propiedad.header = header.nombre;
-                                }
-                            });
-                        });
-
-                        res.send({
-                            status : true,
-                            data : rows,
-                            info : "se muestran todas los inmuebles que hay en la DB"
-                        });
-                    }else{
-                        res.send({
-                            status : false,
-                            info : error
-                        });
-                    }
-                });
-            }else{
-                res.send({
-                    status : false,
-                    info : err
-                });
-            }
-        });
-    }else if(order == "high"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? ORDER BY precio DESC', [idLocalidad, idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idLocalidad = ? AND idCategoria = ? AND idOperacion = ? AND moneda = ? ORDER BY precio ASC', [idLocalidad, idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
@@ -524,10 +487,10 @@ router.get('/filtrar_todo/:idLocalidad/:idCategoria/:idOperacion/:order', (req, 
 
 
 
-router.get('/filtrar_categoria_operacion/:idCategoria/:idOperacion/:order', (req, res) => {
-    const { idCategoria, idOperacion, order }= req.params;
+router.get('/filtrar_categoria_operacion/:idCategoria/:idOperacion/:order/:moneda', (req, res) => {
+    const { idCategoria, idOperacion, order, moneda }= req.params;
     if (order == "normal"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ?', [idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ? AND moneda = ?', [idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
@@ -564,7 +527,7 @@ router.get('/filtrar_categoria_operacion/:idCategoria/:idOperacion/:order', (req
             }
         });
     }else if (order == "low"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ? ORDER BY precio ASC', [idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ? AND moneda = ? ORDER BY precio ASC', [idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
@@ -601,7 +564,7 @@ router.get('/filtrar_categoria_operacion/:idCategoria/:idOperacion/:order', (req
             }
         });
     }else if (order == "high"){
-        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ? ORDER BY precio DESC', [idCategoria, idOperacion], (err, rows, fields) => {
+        db.query('SELECT ubicacion.partido, ubicacion.localidad, tipo_operacion.operacion, categorias.categoria, datos_tecnicos.*, inmuebles.* FROM inmuebles LEFT JOIN ubicacion ON inmuebles.idLocalidad = ubicacion.id LEFT JOIN datos_tecnicos ON inmuebles.id = datos_tecnicos.idCasa LEFT JOIN categorias ON inmuebles.idCategoria = categorias.id LEFT JOIN tipo_operacion ON inmuebles.idOperacion = tipo_operacion.id WHERE idCategoria = ? AND idOperacion = ? AND moneda = ? ORDER BY precio DESC', [idCategoria, idOperacion, moneda], (err, rows, fields) => {
             if(! err){
                 casas = [];
                 rows.forEach(inmueble => {
